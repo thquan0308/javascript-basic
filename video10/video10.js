0a50f5368e38ead805c5656577b83d7e25a60f72
thquan0308 <tranhoangquanthq@gmail.com>
2024-06-11 20:56:56 +0700

    chaper 10 video 10

---
+++ video10/video10.js
@@ -0,0 +1,22 @@
+console.log(`Hello world from HTML`)
+let a = 5; // x <- y
+let b = `5`;
+
+console.log(`compare a == b: `, 'a = ', a, ' b = ', b, ' result: ', a == b)
+console.log(`compare a === b: `, 'a = ', a, ' b = ', b, ' result: ', a === b)
+
+
+// === type, value
+// ==  value
+
+// console.log(`a` + 5 + 4)
+// console.log(5 + 4 + `a`)
+
+// Emty, null, undefined
+
+let c = null
+console.log(`Check value: `, c)
+
+let d = ``
+d = 5
+d = [`11`]
