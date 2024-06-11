d24910e6d607b4506b16312a931c6960888bd929
thquan0308 <tranhoangquanthq@gmail.com>
2024-06-11 18:00:58 +0700

    chaper 8 video 8

---
+++ video8/video8.js
@@ -0,0 +1,27 @@
+console.log(`Hello world from HTML`)
+//key: value
+let obj = {
+    name: `Eric`,
+    address: `Ha Noi`,
+    email: ``,
+    gender: ``,
+    // a: function () {
+    //     console.log(`Hello world inside function`)
+    //     return ``
+    // }
+}
+
+
+
+let b = `address`
+
+// obj[b] = `Harry`
+// obj.b = `Ali`
+obj.name = `HCM`
+
+console.log(`What is your name ? `, `My name is : `, obj.name, obj)
+console.log(`Where are your from ? `, `I'm from : `, obj.address)
+// console.log(`Call function: `, obj.a())
+
+
+// console.log(`type of a: `, typeof a, ` type of b: `, typeof b, ` type of c: `, typeof c)
