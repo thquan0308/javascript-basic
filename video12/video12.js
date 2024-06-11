a093045eaec71c2cdddea97ef05662f40fed095c
thquan0308 <tranhoangquanthq@gmail.com>
2024-06-11 21:28:37 +0700

    chaper 12 video 12

---
+++ video12/video12.js
@@ -0,0 +1,21 @@
+console.log(`Hello world from HTML`)
+let arrTop4 = [`Man City`, `Liverpool`, `MU`, `Chelsea`];
+
+for (let i = 0; i < arrTop4.length; i++) {
+    console.log(`Top: `, i + 1, arrTop4[i])
+}
+console.log(`_________________________`)
+
+// let i = 0
+// while (i < arrTop4.length) {
+//     console.log(`Top: `, i + 1, arrTop4[i])
+//     i++;
+// }
+
+console.log(`_________________________`)
+
+// let i = 0
+// do {
+//     console.log(`Top: `, i + 1, arrTop4[i])
+//     i++;
+// } while (i < 0)
