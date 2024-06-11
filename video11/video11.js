13577572dcecbe5de3ae26805be10ba3c3708b80
thquan0308 <tranhoangquanthq@gmail.com>
2024-06-11 21:11:51 +0700

    chaper 11 video 11

---
+++ video11/video11.js
@@ -0,0 +1,10 @@
+console.log(`Hello world from HTML`)
+
+let i = 0
+let arr = [`MU`, `ManCity`, `Liver`, `Chelsea`]
+for (let i = 0; i < arr.length; i++) {
+    console.log(`check variable i = `, arr[i])
+}
+
+
+console.log(`check value i: `, i)
