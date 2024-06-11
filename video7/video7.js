e931898027e58bc89b77db260660b00719a9d6b8
thquan0308 <tranhoangquanthq@gmail.com>
2024-06-11 16:43:18 +0700

    chaper 7 video 7

---
+++ video7/video7.js
@@ -0,0 +1,10 @@
+console.log('Hello world from HTML')
+
+let a = 5;
+let b = 10;
+console.log("Fifteen is " + (a + b) + " and\nnot " + (2 * a + b) + ".");
+console.log("Fifteen is " + a + b + " and\nnot " + (2 * a + b) + ".");
+
+// url: 'hoidanit/5'
+console.log(`Hoi dan IT check strings: a = ${a}; b = ${b}; a + b = ${a + b}`)
+console.log(`/hoidanit/${a}`)
