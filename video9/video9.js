4f9edaf6b7f7cac9a3c517d9f6769143c750fdca
thquan0308 <tranhoangquanthq@gmail.com>
2024-06-11 20:31:41 +0700

    chaper 9 video 9

---
+++ video9/video9.js
@@ -0,0 +1,29 @@
+console.log(`Hello world from HTML`)
+
+let a = {
+    name: `Eric`,
+    age: 25,
+}; //key:value
+
+console.log(`Check data object: `, a)
+
+console.log(`My name's : `, a.name)
+
+//arr = [item1, item2, item3]
+let b = [`MU`, `Chelsea`, `Liver`]
+console.log(`type of b`, b)
+
+console.log(`first item: `, b[0], `second item: `, b[1])
+
+let
+    family = [{
+            name: `Eric`,
+            age: 25
+        },
+        {
+            name: `Monica`,
+            age: 24
+        }
+    ]
+
+console.log(`My family : `, family)
