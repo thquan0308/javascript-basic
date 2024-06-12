09ab22663c680e352091c65dfc73740703a5d0d1
thquan0308 <tranhoangquanthq@gmail.com>
2024-06-12 22:28:22 +0700

    chaper 17 video 17

--- video16/video16.js
+++
@@ -1,9 +0,0 @@
-console.log(`Hello world from HTML`)
-
-//input parameter
-function sum(a, b) {
-    return a + b;
-}
-
-let c = sum(10, 20);
-console.log(`sum a + b = `, c)
---
+++ video17/video17.js
@@ -0,0 +1,14 @@
+console.log(`Hello world from HTML`)
+
+function ericFunc(x, y, i) {
+    console.log(`1`);
+    console.log(`2`);
+    console.log(`3`);
+    if (i === 5) return;
+    console.log(`4`);
+    console.log(`5`);
+
+    return x + y;
+}
+
+console.log(`Check sum : `, ericFunc(9, 6, 5))
