0c8f067b88242abde2967f353f60efbe576c9a5d
thquan0308 <tranhoangquanthq@gmail.com>
2024-06-12 22:17:20 +0700

    chaper 16 video 16

--- video15/video15.js
+++
@@ -1,7 +0,0 @@
-console.log(`Hello world from HTML`)
-
-
-
-for (let i = 0; i < 10; i++) {
-    console.log(`>>> Check value i : `, i)
-}
---
+++ video16/video16.js
@@ -0,0 +1,9 @@
+console.log(`Hello world from HTML`)
+
+//input parameter
+function sum(a, b) {
+    return a + b;
+}
+
+let c = sum(10, 20);
+console.log(`sum a + b = `, c)
