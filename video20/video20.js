36d6cadb41ed0ead0bf0a31224af5c9d685f87d4
thquan0308 <tranhoangquanthq@gmail.com>
2024-06-14 21:57:38 +0700

    chaper 20 video 20

---
+++ video20/video20.js
@@ -0,0 +1,31 @@
+console.log(`Hello world from HTML`)
+
+let sum = (a, b, callback) => {
+
+    let tong = a + b;
+
+    // setTimeout(() => {
+    //     callback(tong);
+    // }, 5000)
+
+    let i = 0;
+    let timer = setInterval(() => {
+        callback(tong);
+        i++;
+        console.log(`>>> i: `, i)
+
+        if (i === 5) {
+            clearInterval(timer)
+        }
+    }, 1000);
+
+}
+
+
+
+let printSum = (message) => {
+
+    console.log(`>>> check sum: 6 + 9 = `, message)
+}
+
+sum(6, 9, printSum)
