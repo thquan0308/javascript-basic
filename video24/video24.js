cefde79533f7da51a0055862f3ddf0d6f16d901b
thquan0308 <tranhoangquanthq@gmail.com>
2024-06-15 17:33:43 +0700

    chaper 24 video 24

---
+++ video24/video24.js
@@ -0,0 +1,15 @@
+console.log(`Hello world from HTML`);
+
+const items = [
+  { name: "Edward", value: 21 },
+  { name: "Sharpe", value: 37 },
+  { name: "And", value: 45 },
+  { name: "The", value: -12 },
+  { name: "Magnetic", value: 13 },
+  { name: "Zeros", value: 37 },
+];
+
+// sort by value
+items.sort((a, b) => a.value - b.value);
+
+console.log(items);
